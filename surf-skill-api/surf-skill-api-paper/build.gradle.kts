94f69bb9c63f391376183483403f3f3a1aa6b36c
Keviro <25409956+Keviro@users.noreply.github.com>
2026-05-02 01:13:35 +0200

    Add optional AuxProtect compile dependency

--- surf-skill-api/surf-skill-api-paper/build.gradle.kts
+++ surf-skill-api/surf-skill-api-paper/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("dev.slne.surf.api.gradle.paper-raw")
 }
 
+repositories {
+    maven("https://jitpack.io")
+}
+
 dependencies {
     api(projects.surfSkillApi.surfSkillApiCommon)
+    compileOnly("com.github.Heliosares:AuxProtect:1.3.1")
 }
