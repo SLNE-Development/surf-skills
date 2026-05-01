87e89fb66e29cf97fa5cdf39695e18bc387815eb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-01 22:54:20 +0200

    Revert "refactor(mining): add check for Silk Touch enchantment in experience collection"
    This reverts commit cb3ce41c59d5ecc2f02307110c64e833326183b9.

--- surf-skill-core/surf-skill-core-paper/src/main/kotlin/dev/slne/surf/skill/core/paper/skills/mining/listeners/MiningBlockListener.kt
+++ surf-skill-core/surf-skill-core-paper/src/main/kotlin/dev/slne/surf/skill/core/paper/skills/mining/listeners/MiningBlockListener.kt
@@ -12,7 +12,6 @@ import dev.slne.surf.skill.core.paper.util.SkillLevelingHandler
 import dev.slne.surf.skill.core.paper.util.isEligibleForExperience
 import org.bukkit.Material
 import org.bukkit.block.Block
-import org.bukkit.enchantments.Enchantment
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -53,10 +52,6 @@ object MiningBlockListener : Listener {
         var exp = blockExpMap[block.type] ?: return
 
         if (!block.isEligibleForExperience()) {
-            return
-        }
-
-        if (event.player.inventory.itemInMainHand.containsEnchantment(Enchantment.SILK_TOUCH)) {
             return
         }
 
