243012b277979506e81895fb49fe83e204be49e9
Jan <jan.hoeck@gmx.net>
2026-05-02 13:29:04 +0200

    feat(stats): hook saveStats into SkillPlayerManagerImpl.savePlayer

--- surf-skill-core/surf-skill-core-paper/src/main/kotlin/dev/slne/surf/skill/core/paper/player/SkillPlayerManagerImpl.kt
+++ surf-skill-core/surf-skill-core-paper/src/main/kotlin/dev/slne/surf/skill/core/paper/player/SkillPlayerManagerImpl.kt
@@ -6,6 +6,7 @@ import com.sksamuel.aedile.core.asLoadingCache
 import dev.slne.surf.skill.api.paper.player.SkillPlayer
 import dev.slne.surf.skill.api.paper.player.SkillPlayerManager
 import dev.slne.surf.skill.core.paper.experience.ExperienceService
+import dev.slne.surf.skill.core.paper.stats.SkillStatsHook
 import net.kyori.adventure.util.Services
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
@@ -36,6 +37,7 @@ class SkillPlayerManagerImpl : SkillPlayerManager, Services.Fallback {
 
     override suspend fun savePlayer(player: SkillPlayer) {
         ExperienceService.savePlayerExperience(player.uuid, player.experiences)
+        SkillStatsHook.pushCurrent(player)
     }
 
     override fun invalidatePlayer(uuid: UUID) {
