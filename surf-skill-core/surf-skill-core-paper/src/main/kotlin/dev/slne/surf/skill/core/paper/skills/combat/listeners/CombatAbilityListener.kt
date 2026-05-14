3cd1fd639537f5b6431c5252eef638f6d661d7c2
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-05-14 10:23:22 +0200

    fix(combat): remove logging for Reaper's Fortune and extra drops in CombatAbilityListener

--- surf-skill-core/surf-skill-core-paper/src/main/kotlin/dev/slne/surf/skill/core/paper/skills/combat/listeners/CombatAbilityListener.kt
+++ surf-skill-core/surf-skill-core-paper/src/main/kotlin/dev/slne/surf/skill/core/paper/skills/combat/listeners/CombatAbilityListener.kt
@@ -1,6 +1,5 @@
 package dev.slne.surf.skill.core.paper.skills.combat.listeners
 
-import dev.slne.surf.api.core.messages.adventure.sendText
 import dev.slne.surf.api.core.util.random
 import dev.slne.surf.skill.api.paper.skills.CombatSkill
 import dev.slne.surf.skill.core.paper.ability.AbilityUtil
@@ -107,7 +106,6 @@ object CombatAbilityListener : Listener {
         )
 
         if (!AbilityUtil.rollChance(chance)) {
-            println("Reaper's Fortune did not trigger. Chance: $chance")
             return
         }
 
@@ -125,13 +123,6 @@ object CombatAbilityListener : Listener {
                 rerolled.isSimilar(original)
             }
         }.map { it.clone() }
-
-        println("DROPS: ${event.drops.size}, EXTRA DROPS: ${extraDrops.size}")
-
-        killer.sendText {
-            appendSuccessPrefix()
-            success("Extra Drops: ${extraDrops.joinToString { it.type.toString() }}")
-        }
 
         event.drops += extraDrops
     }
